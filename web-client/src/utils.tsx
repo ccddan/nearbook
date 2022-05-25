b1447f9cb392456245b8831fea31ee5f2bc471af
ccddan <ccdd.dev@gmail.com>
2022-05-25 15:38:24 -0500

    ref: do not used deprecated props

--- web-client/src/utils.tsx
+++ web-client/src/utils.tsx
@@ -8,28 +8,20 @@ const nearConfig = getConfig(process.env.NODE_ENV || "development");
 // Initialize contract & set global variables
 export async function initContract() {
   // Initialize connection to the NEAR testnet
-  const near = await connect(
-    Object.assign(
-      { deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } },
-      nearConfig
-    )
-  );
+  const near = await connect({
+    keyStore: new keyStores.BrowserLocalStorageKeyStore(),
+    ...nearConfig,
+  });
 
-  // Initializing Wallet based Account. It can work with NEAR testnet wallet that
-  // is hosted at https://wallet.testnet.near.org
   window.walletConnection = new WalletConnection(near, null);
 
-  // Getting the Account ID. If still unauthorized, it's just empty string
   window.accountId = window.walletConnection.getAccountId();
 
-  // Initializing our contract APIs by contract name and configuration
   window.contract = await new Contract(
     window.walletConnection.account(),
     nearConfig.contractName,
     {
-      // View methods are read only. They don't modify the state, but usually return some value.
       viewMethods: ["getGreeting"],
-      // Change methods can modify the state. But you don't receive the returned value when called.
       changeMethods: ["setGreeting"],
     }
   );
@@ -40,9 +32,5 @@ export function logout() {
 }
 
 export function login() {
-  // Allow the current app to make calls to the specified contract on the
-  // user's behalf.
-  // This works by creating a new access key for the user's account and storing
-  // the private key in localStorage.
   window.walletConnection.requestSignIn(nearConfig.contractName);
 }
