2096a6e1f591a19e8d9dc6b7563caae0dae46be4
ccddan <ccdd.dev@gmail.com>
2022-05-25 19:54:30 -0500

    feat: add tailwind main colors

--- web-client/tailwind.config.js
+++ web-client/tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      dark: "#1e1e1e",
+      gray: "#555",
+      light: "#ccc",
+      shadow: "#e6e6e6",
+      success: "rgb(90, 206, 132)",
+      error: "#FF585D",
+      warning: "#fff200",
+      primary: "#9c3c3f",
+      secondary: "#3E606F"
+    },
     extend: {},
   },
   plugins: [],
