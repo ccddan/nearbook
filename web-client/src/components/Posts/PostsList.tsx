2482d88b9daa0b2752a838824903247d6811c95d
ccddan <ccdd.dev@gmail.com>
2022-05-29 12:23:34 -0500

    fix: add key to each rendered post card

--- web-client/src/components/Posts/PostsList.tsx
+++ web-client/src/components/Posts/PostsList.tsx
@@ -10,8 +10,8 @@ export const PostsList = (props: PostsListProps) => {
     <section>
       <div className="px-4 py-8 container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 mt-8 lg:grid-cols-3 md:gap-x-4 gap-x-2 gap-y-8">
-          {posts.map((item: any) => (
-            <PostCard post={item} />
+          {posts.map((item: any, idx: number) => (
+            <PostCard key={idx} post={item} />
           ))}
         </div>
       </div>
