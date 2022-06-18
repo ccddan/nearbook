147294b1714359c8d909a78b43ea9e8e5666a26f
ccddan <ccdd.dev@gmail.com>
2022-06-17 21:30:24 -0500

    test: render static tag

--- web-client/src/App.test.tsx
+++ web-client/src/App.test.tsx
@@ -1,10 +1,12 @@
-import { render, screen } from "@testing-library/react";
+import React from 'react';
 
-import App from "./App";
-import React from "react";
+import {
+  render,
+  screen,
+} from '@testing-library/react';
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<p>App</p>);
+  const linkElement = screen.getByText(/App/i);
   expect(linkElement).toBeInTheDocument();
 });
